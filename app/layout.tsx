0217a45817058195860c6f95886555c769aadd70
wyllieonchain <45154279+wyllieonchain@users.noreply.github.com>
2024-02-24 21:43:30 +0000

    testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ export const viewport = {
   width: 'device-width',
   initialScale: 1.0,
 };
-
+// testing
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
